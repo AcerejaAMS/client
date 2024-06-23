ba5d4340dd28b2347958d59d32be29449a710fb6
AcerejaAMS <amoraess1412@gmail.com>
2024-06-23 04:36:55 -0600

    Correciones proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+import PiePublico from './pages/publicas/PiePaginaPublico.js';
+
+const App = () => {
+  
+  const cuerpoInfo = {
+    display: 'flex',
+    justifyContent: 'space-between',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PiePublico />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/publicas/PiePaginaPublico.js
@@ -0,0 +1,16 @@
+import './aparienciaPublica.css';
+
+const PiePublico = () => {
+    const pie = (
+        <div className="piePagina">
+            <p>Avisos de Privacidad</p>
+            <p>&nbsp;|&nbsp;</p>
+            <p>Términos y condiciones</p>
+            <p>&nbsp;|&nbsp;</p>
+            <p>Acerca de Nosotros</p>
+        </div>
+    );
+    return(pie);
+};
+
+export default PiePublico;
